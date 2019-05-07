a749bd007dd6174162e129c97f842c348df1b40b
ThayPedroso <thayla.pedroso88@gmail.com>
2019-05-07 20:46:37 -0300

    Aplicação básica com a implementação de criação, leitura, alteração e exclusão de registros de produtos em um estoque.

---
+++ backend.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+class Database:
+
+    def __init__(self):
+        self.conn = sqlite3.connect("Products.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS product (id INTEGER PRIMARY KEY, description text, manufacturer text, quantity integer, code text, barCode integer)")
+        self.conn.commit()
+
+    def insert(self,description,manufacturer,quantity,code,barCode):
+        self.cur.execute("INSERT INTO product VALUES (NULL,?,?,?,?,?)",(description,manufacturer,quantity,code,barCode))
+        self.conn.commit() 
+
+    def view(self):
+        self.cur.execute("SELECT * FROM product")
+        rows=self.cur.fetchall()   
+        return rows
+
+    def search(self, description="",manufacturer="",quantity="",code="",barCode=""):
+        self.cur.execute("SELECT * FROM product WHERE description=? OR manufacturer=? OR quantity=? OR code=? OR barCode=?",(description,manufacturer,quantity,code,barCode))
+        rows=self.cur.fetchall()
+        return rows  
+
+    def delete(self, id):
+        self.cur.execute("DELETE FROM product WHERE id=?",(id,))
+        self.conn.commit() 
+
+    def update(self, id,description,manufacturer,quantity,code,barCode):
+        self.cur.execute("UPDATE product SET description=?, manufacturer=?, quantity=?, code=?, barCode=? WHERE id=?",(description,manufacturer,quantity,code,barCode,id))
+        self.conn.commit()
+
+    def __del__ (self):
+        self.conn.close()
+
